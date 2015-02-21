d970c71ec6a68d11ddcab50dc3b84e346ae7a24b
Michael Spoehr <spoehrmichael@gmail.com>
2015-02-20 20:04:02 -0500

    Added VirtualizablePsuedoDigitalInput

---
+++ src/com/coderedrobotics/libs/VirtualizablePsuedoDigitalInput.java
@@ -0,0 +1,16 @@
+package com.coderedrobotics.libs;
+
+/**
+ *
+ * @author michael
+ */
+public class VirtualizablePsuedoDigitalInput extends VirtualizableAnalogInput {
+
+    public VirtualizablePsuedoDigitalInput(int port) {
+        super(port);
+    }
+
+    public boolean getState() {
+        return super.get() > 2;
+    }
+}
