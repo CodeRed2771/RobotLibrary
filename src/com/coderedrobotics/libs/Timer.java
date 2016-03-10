abd8dedf7397cd7c9cd189a919b5f6b6228893c0
dvanvoorst <dale@dalevanvoorst.com>
2016-03-09 22:06:32 -0500

    Added new methods to Timer class

--- src/com/coderedrobotics/libs/Timer.java
+++ src/com/coderedrobotics/libs/Timer.java
@@ -3,6 +3,13 @@ package com.coderedrobotics.libs;
 /**
  *
  * @author Michael
+ * 3/9/16 - revised by D. Van Voorst
+ * This class can be used to simplify timing autonomous steps
+ * Use getStage() to manage your state machine
+ * Use setTimerAndAdvanceStage() to start a timer for a particular stage
+ * Use advanceWhenReady() to automatically advance to the next auto stage when the timer is reached
+ * Use stopTimeAndAdvanceStage() when your drive command has reached its target to prevent the advanceWhenReady from firing also
+ * 
  */
 public class Timer {
 
@@ -17,8 +24,18 @@ public class Timer {
         startTime = System.currentTimeMillis() + time;
     }
 
+    public void setTimerAndAdvanceStage(long time) {
+    	resetTimer(time);
+    	nextStage();
+    }
+    
+    public void stopTimerAndAdvanceStage() {
+    	resetTimer(1000000); // push the target time way into the future so it never reaches the target time
+    	nextStage();
+    }
+    
     public void advanceWhenReady() {
-        if (startTime < System.currentTimeMillis()) {
+        if (ready()) {
             stage++;
         }
     }
